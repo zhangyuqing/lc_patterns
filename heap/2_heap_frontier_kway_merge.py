a9b6c09b03ea968b98c2c8e56880f45bea075833
Yuqing Zhang <8079770+zhangyuqing@users.noreply.github.com>
2026-09-14 12:25:08 -0700

    merge k lists

--- heap/2_heap_frontier_kway_merge.py
+++ heap/2_heap_frontier_kway_merge.py
@@ -1,3 +1,33 @@
 # LC 23: https://leetcode.com/problems/merge-k-sorted-lists/description/
 
-from heapq import 
+from lc_patterns.utils.list_node import ListNode
+from typing import List, Optional
+from heapq import heappush, heappop
+
+
+def mergeKLists(lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    if len(lists) == 0:
+        return None
+
+    h = []
+    for i in range(len(lists)):
+        if lists[i]:
+            heappush(h, (lists[i].val, i))
+    if len(h) == 0:
+        return None
+
+    _, root_idx = heappop(h)
+    root = lists[root_idx]
+    lists[root_idx] = lists[root_idx].next
+    if lists[root_idx]:
+        heappush(h, (lists[root_idx].val, root_idx))
+
+    p = root
+    while h:
+        _, idx = heappop(h)
+        p.next = lists[idx]
+        p = p.next
+        lists[idx] = lists[idx].next
+        if lists[idx]:
+            heappush(h, (lists[idx].val, idx))
+    return root
--- tree/3_postorder_example.py
+++ tree/3_postorder_example.py
@@ -1,7 +1,7 @@
 # LC 543: https://leetcode.com/problems/diameter-of-binary-tree/
 
-from typing import Optional
 from lc_patterns.utils.tree_node import TreeNode
+from typing import Optional
 
 class DiameterOfBinaryTree:
     def __init__(self):    
