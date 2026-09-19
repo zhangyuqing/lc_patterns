d09861d5970e03db7dd44c6f357e6928791d0362
Yuqing Zhang <8079770+zhangyuqing@users.noreply.github.com>
2026-09-18 17:38:19 -0700

    graph traversal

---
+++ graph/1_traversal_iterative.py
@@ -0,0 +1,48 @@
+# Iterative DFS & BFS template
+# - pop
+# - mark visited
+# - processing
+# - expand & push
+# 
+# def search(start):
+#     visited = set()
+#     frontier = [(start, other info...)]     # queue for BFS, stack for DFS
+# 
+#     while frontier:
+#         node = frontier.pop(...)
+#         if node not in visited:
+#             visited.add(node)
+#             
+#             # process node
+#
+#             for nb in neighbors(node):
+#                 frontier.push(nb)
+
+# Recursive DFS
+# def dfs(node):
+#     visited.add(node)
+#     for nb in neighbors(node):
+#         if nb not in visited:
+#             dfs(nb)
+
+# LC 841: https://leetcode.com/problems/keys-and-rooms/description/
+def canVisitAllRooms(rooms: list[list[int]]) -> bool:
+    # build directed graph
+    n = len(rooms)
+    graph = {i:rooms[i] for i in range(n)}
+    # u: [v] - there's a directed edge from u to v
+
+    visited = set()
+    frontier = [0]
+    while frontier:
+        node = frontier.pop()
+
+        if node not in visited:
+            visited.add(node)
+
+            for nb in graph[node]:
+                frontier.append(nb)
+    
+    if len(visited) == n:
+        return True
+    return False
